79065ddc30bc045ec4f4bfcc71e9e6d425a28a1f
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-03 15:54:24 +0300

    add index action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,8 @@
 class RestaurantsController < ApplicationController
+    before_action :set_restaurant, only: %i[ show edit update destroy ]
+
+    # GET /restaurants or /restaurants.json
+    def index
+      @restaurants = Restaurant.all
+    end
 end
