d100da959c3960147052c56bb0556340bb95c5bf
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-03 13:51:41 +0300

    add create, update, delete and pizza params

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -11,3 +11,67 @@ end
 
 def show
 end
+
+def new
+  @pizza = Pizza.new
+end
+
+
+def edit
+end
+
+def create
+  @pizza = Pizza.new(pizza_params)
+
+  respond_to do |format|
+    if @pizza.save
+      format.html { redirect_to pizza_url(@pizza), notice: "Pizza was successfully created." }
+      format.json { render :show, status: :created, location: @pizza }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @pizza.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+
+def update
+  respond_to do |format|
+    if @pizza.update(pizza_params)
+      format.html { redirect_to pizza_url(@pizza), notice: "Pizza was successfully updated." }
+      format.json { render :show, status: :ok, location: @pizza }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @pizza.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+# DELETE
+def destroy
+  @pizza.destroy
+
+  respond_to do |format|
+    format.html { redirect_to pizzas_url, notice: "Pizza was successfully destroyed." }
+    format.json { head :no_content }
+  end
+end
+
+def record_not_found
+  # flash[:notice] = "Wrong post it"
+  # render 'record_not_found'
+  render html: {error: "record not found"}, status: :unprocessable_entity
+end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pizza
+    @pizza = Pizza.find(params[:id])
+
+  end
+
+  # Only allow a list of trusted parameters through.
+  def pizza_params
+    params.require(:pizza).permit(:name, :ingredients)
+  end
+end
