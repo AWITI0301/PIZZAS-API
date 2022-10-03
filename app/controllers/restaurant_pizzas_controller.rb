d113413d71872d7288decb055439c76f591e1050
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-03 14:23:18 +0300

    add index, show new and edit actions

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantPizzasController < ApplicationController
+    before_action :set_restaurant_pizza, only: %i[ show edit update destroy ]
+
+    # GET /restaurant_pizzas or /restaurant_pizzas.json
+    def index
+      @restaurant_pizzas = RestaurantPizza.all
+    end
+  
+    # GET /restaurant_pizzas/1 or /restaurant_pizzas/1.json
+    def show
+    end
+  
+    # GET /restaurant_pizzas/new
+    def new
+      @restaurant_pizza = RestaurantPizza.new
+    end
+  
+    # GET /restaurant_pizzas/1/edit
+    def edit
+    end
 end
