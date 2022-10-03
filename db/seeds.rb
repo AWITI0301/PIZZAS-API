3bc5607ee3ce5da3a58ce1fba0914b3671f5e1fc
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-04 00:13:20 +0300

    deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@
 Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
 Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
 
+
 Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
 Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
+
 
 RestaurantPizza.create(price: 5, pizza_id: 1, restaurant_id: 3)
