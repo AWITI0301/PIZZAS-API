4cd752e5e4d36a26ce77ff8eaceb9f5db6c5c823
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-03 13:32:10 +0300

    add action index for the controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,10 @@
 class PizzasController < ApplicationController
 end
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+before_action :set_pizza, only: %i[ show edit update destroy ]
+
+
+def index
+  @pizzas = Pizza.all
+end
